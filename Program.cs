e091efa8f15898058a86bddbe59b53630341d123
Isaac Jordan <isaaz.jordan02@gmail.com>
2024-12-19 18:10:56 -0300

    feat : Initial commit of base files, jwt simple implementation for practice

---
+++ Program.cs
@@ -0,0 +1,99 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add JWT Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "isaac",
+        ValidAudience = "cualquiera",
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("llave_super_secreta_no_mostrar_a_nadie_Xd"))
+    };
+    options.Events = new JwtBearerEvents
+    {
+        OnAuthenticationFailed = context =>
+        {
+            Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+            return Task.CompletedTask;
+        },
+        OnTokenValidated = context =>
+        {
+            Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+            return Task.CompletedTask;
+        }
+    };
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("UserPolicy", policy => policy.RequireRole("User"));
+    options.AddPolicy("ApprenticePolicy", policy => policy.RequireRole("Apprentice"));
+    options.AddPolicy("ExpertPolicy", policy => policy.RequireRole("Expert"));
+    options.AddPolicy("AdminPolicy", policy => policy.RequireRole("Admin"));
+});
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger Test", Version = "v1" });
+    
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter JWT with Bearer into field",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+    {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            }
+        },
+        new string[] { }
+    }});
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1");
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ RoleController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using jwt_practice;
+
+[ApiController]
+[Route("[controller]")]
+
+//[Authorize(Policy = "ApprenticePolicy")] // A todo el controlador
+public class RoleController : ControllerBase
+{
+    [Authorize(Policy = "UserPolicy")]
+    [HttpGet("user")]
+    public IActionResult GetUser()
+    {
+        return Ok("User endpoint");
+    }
+
+    [Authorize(Policy = "ApprenticePolicy")]
+    [HttpGet("apprentice")]
+    public IActionResult GetApprentice()
+    {
+        return Ok("Apprentice endpoint");
+    }
+
+    [Authorize(Policy = "ExpertPolicy")]
+    [HttpGet("expert")]
+    public IActionResult GetExpert()
+    {
+        return Ok("Expert endpoint");
+    }
+
+    [Authorize(Policy = "AdminPolicy")]
+    [HttpGet("admin")]
+    public IActionResult GetAdmin()
+    {
+        return Ok("Admin endpoint");
+    }
+
+    [AllowAnonymous]
+    [HttpPost("login_to_get_token")]
+    public IActionResult LoginToGetToken([FromBody] LoginRequest request)
+    {
+        if (request.Username == "isaac" && request.Password == "isaac123")
+        {
+            var token = GenerateJwtToken(request.Role);
+            return Ok(new { Token = token });
+        }
+        return Unauthorized(new { message = "Invalid credentials" });
+    }
+
+    private string GenerateJwtToken(string role)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.UTF8.GetBytes("llave_super_secreta_no_mostrar_a_nadie_Xd");
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Role, role),
+                new Claim(JwtRegisteredClaimNames.Aud, "cualquiera"),
+                new Claim(JwtRegisteredClaimNames.Iss, "isaac") 
+            }),
+            Expires = DateTime.UtcNow.AddHours(1),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
